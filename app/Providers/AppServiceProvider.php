fbb7b68049e573729cc4b3da8b37dedc34cce809
Jesus Gerardo <jesus.gerardo.mn@outlook.com>
2017-10-15 16:51:59 -0600

    heroku deployment stuff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,4 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    // Force SSL in production
+    /*if ($this->app->environment() == 'production') {
+    	URL::forceScheme('https');
+    }*/
 }
